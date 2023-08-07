d20c42d808003e4f1fe80b79a05674c8fa959bed
Lukas Dombrovsky <lukas.dombrovsky@gmail.com>
2023-08-07 19:33:48 +0200

    Navbar active links colors

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import NavigationBar from "./components/UI/NavigationBar";
-import Home from "./components/sections/Home";
-import Section from "./components/UI/Section";
-import About from "./components/sections/About";
-import Projects from "./components/sections/Projects";
-import TechStack from "./components/sections/TechStack";
-import Contact from "./components/sections/contact/Contact";
+import NavigationBar from './components/UI/NavigationBar';
+import Home from './components/sections/Home';
+import Section from './components/UI/Section';
+import About from './components/sections/About';
+import Projects from './components/sections/Projects';
+import TechStack from './components/sections/TechStack';
+import Contact from './components/sections/contact/Contact';
 
-import "./App.scss";
+import './App.scss';
 
 function App() {
   return (
     <>
       <NavigationBar
-        brand="Lukas Dombrovsky"
-        homeLink="root"
-        links={["home", "about", "projects", "techStack", "contact"]}
-        expand="md"
-        colors={{
-          notScrolled: { backgroundColor: "black", textColor: "white" },
-          scrolled: { backgroundColor: "white", textColor: "black" },
-        }}
-        activeTextColor="#237acc"
+        brand='Lukas Dombrovsky'
+        homeLink='home'
+        links={['about', 'projects', 'techStack', 'contact']}
+        expand='md'
       />
-      <Home id="home" />
-      <Section id="about">
+      <Home id='home' />
+      <Section id='about'>
         <About />
       </Section>
-      <Section id="projects">
+      <Section id='projects'>
         <Projects />
       </Section>
-      <TechStack id="techStack" />
-      <Section id="contact">
+      <TechStack id='techStack' />
+      <Section id='contact'>
         <Contact />
       </Section>
     </>
--- src/components/UI/NavigationBar.js
+++ src/components/UI/NavigationBar.js
@@ -1,81 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import { NavHashLink } from "react-router-hash-link";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
-import classes from "./NavigationBar.module.scss";
+import classes from './NavigationBar.module.scss';
 
 const NavigationBar = (props) => {
   const [pageScrolled, setPageScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState();
 
   useEffect(() => {
     const handleScroll = () => {
       setPageScrolled(window.scrollY > 50);
     };
 
-    document.addEventListener("scroll", handleScroll);
+    document.addEventListener('scroll', handleScroll);
 
     return () => {
-      document.removeEventListener("scroll", handleScroll);
+      document.removeEventListener('scroll', handleScroll);
     };
-  }, [pageScrolled]);
+  }, []);
+
+  const linkClickHandler = (event) => {
+    setActiveLink(event.target.innerText);
+  };
 
   const links = props.links.map((link) => (
-    <NavHashLink
+    <a
       key={link}
-      className={classes["nav-link"]}
-      style={{
-        color: pageScrolled
-          ? props.colors.scrolled.textColor
-          : props.colors.notScrolled.textColor,
-      }}
-      smooth
-      activeStyle={{ color: props.activeTextColor }}
-      to={`/#${link}`}
+      className={`${classes.link} ${
+        activeLink === link ? classes.active : ''
+      } ${pageScrolled ? classes.scrolled : ''}`}
+      onClick={linkClickHandler}
+      href={`#${link}`}
     >
       {link}
-    </NavHashLink>
+    </a>
   ));
 
   return (
     <Navbar
-      className={classes.navbar}
-      variant={pageScrolled ? "light" : "dark"}
+      className={`${classes.navbar} ${pageScrolled && classes.scrolled}`}
       expand={props.expand}
-      fixed="top"
-      style={{
-        backgroundColor: pageScrolled
-          ? props.colors.scrolled.backgroundColor
-          : props.colors.notScrolled.backgroundColor,
-        borderBottom: pageScrolled
-          ? props.colors.scrolled.borderBottom
-          : props.colors.notScrolled.borderBottom,
-      }}
+      fixed='top'
     >
       <Container fluid={props.fluid}>
         <Navbar.Brand
-          className={classes["navbar-brand"]}
-          href={props.homeLink}
-          style={{
-            color: pageScrolled
-              ? props.colors.scrolled.textColor
-              : props.colors.notScrolled.textColor,
-          }}
+          className={`${classes.brand} ${pageScrolled && classes.scrolled}`}
+          href={`#${props.homeLink}`}
+          onClick={linkClickHandler}
         >
-          {typeof props.brand === "string" &&
-          (props.brand.includes("jpg") || props.brand.includes("png")) ? (
-            <img src={props.brand} alt="Application logo" />
+          {typeof props.brand === 'string' &&
+          (props.brand.includes('jpg') || props.brand.includes('png')) ? (
+            <img src={props.brand} alt='Application logo' />
           ) : (
             props.brand
           )}
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse
-          className={classes["navbar-collapse"]}
-          id="basic-navbar-nav"
-        >
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse className={classes.collapse} id='basic-navbar-nav'>
           <Nav>{links}</Nav>
         </Navbar.Collapse>
       </Container>
